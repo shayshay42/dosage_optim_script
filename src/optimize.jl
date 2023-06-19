2e1a04d516540342a3ac3d852ac0643d1d0e8d72
DrShay <78826185+popsicleinhaler@users.noreply.github.com>
2023-06-19 17:44:10 -0400

    sgd with single callback

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,7 +1,7 @@
-using DifferentialEquations, Zygote, SciMLSensitivity, Optimization, Optim, OptimizationOptimisers
+using DifferentialEquations, Zygote, SciMLSensitivity, Optimization, OptimizationOptimisers, OptimizationOptimJL
 
 include("../assets/model.jl")
-include("../assets/dosing.jl")
+include("../assets/dosing_unified.jl")
 include("../assets/params.jl")
 include("../assets/utils.jl")
 
@@ -10,16 +10,17 @@ tspan = (0,end_time+7).*hours
 p = [ode_params;doses]
 prob = ODEProblem(pk_pd!,u0,tspan,p)
 
-sensealg = ForwardDiffSensitivity(convert_tspan=true)
+sensealg = ForwardDiffSensitivity()#(convert_tspan=true)
 # cb2 = SciMLSensitivity.track_callbacks(hit, prob.tspan[1], prob.u0, prob.p, sensealg)
 
 function loss(θ)
-    tmp_prob = remake(prob, p=[ode_params;θ])
+    q = [ode_params;θ]
+    tmp_prob = remake(prob, p=q)
     cb2 = SciMLSensitivity.track_callbacks(hit, tmp_prob.tspan[1], tmp_prob.u0, tmp_prob.p, sensealg)
-    tmp_sol = solve(tmp_prob, p=[ode_params;θ], callback=cb2, abstol=1e-12, reltol=1e-12, sensealg=sensealg)
+    tmp_sol = solve(tmp_prob, p=q, callback=cb2, abstol=1e-12, reltol=1e-12, sensealg=sensealg)
     sols = convert(Array,tmp_sol)
     c = sols[1,:][end]
-    return c + sum(abs, θ)/(1800*(length(θ)))
+    return c + sum(abs, θ)/(1800.0*avg_huma_surface_area*(length(θ)))
 end
 
 loss(doses)
@@ -29,7 +30,7 @@ loss(doses)
 adtype = Optimization.AutoZygote()
 optf = Optimization.OptimizationFunction((x,_)->loss(x), adtype)
 optprob = Optimization.OptimizationProblem(optf, doses)
-callback, loss_values = create_callback()
-opt = ADAM(0.1)
-@time res = Optimization.solve(optprob, opt, callback = callback, maxiters=1)
+callback, loss_values, iter = create_callback()
+opt = ADAMW(0.1)#ADAM(0.1)
+@time res = Optimization.solve(optprob, opt, callback = callback, maxiters=50)
 @show res.u
